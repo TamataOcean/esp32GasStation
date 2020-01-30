712e985b7eb832c6678dc73b8465269578dbcf24
Romain Tourte <rtourte@yahoo.fr>
2020-01-30 09:26:16 +0100

    NodeJs Daemon ready to record

--- systools/actions/components/TamataInflux.js
+++ systools/actions/components/TamataInflux.js
@@ -6,129 +6,83 @@ const FieldType = Influx.FieldType;
 
 
 class TamataInfluxDB {
-   constructor ( jsonObject, measurement ) {
-   	this.measurement = measurement;
-   	this.config = jsonObject;
-
-   	console.log('InfluxDB constructor...');
-   	console.log('Config = ' + JSON.stringify ( jsonObject ) );
-   	console.log('InfluxDB Measurement = ' + measurement);
-   	this.connect()
+   constructor ( jsonObject ) {
+      this.config = jsonObject;
+      console.log('InfluxDB constructor...');
+      console.log('Config = ' + JSON.stringify ( jsonObject ) );
+      this.connect()
    }
 
    connect() {
-   	console.log("InfluxDB - connect function");
-   	if ( this.measurement === "sensor") {
-   		console.log('this a sensor to save to InfluxDB');
-   		this.influx = new Influx.InfluxDB({
-   	        database: this.config.database,
-   	        host: this.config.host,
-   	        port: this.config.port,
-   	        username: this.config.username,
-   	        password: this.config.password,
-   	        schema: [
-   	          {
-   	            measurement: this.measurement,
-   	            fields: {
-   	              user :         FieldType.STRING,
-   	              timestamp :  FieldType.FLOAT,
-   	              CO2: FieldType.FLOAT,
-                    TVOC: FieldType.FLOAT
-   	            },
-   	            tags: [ 'sensor' ]
-   	          }
-   	        ]
-   	    });
-   	}
+      console.log("InfluxDB - connect function");
+      console.log('this a sensor to save to InfluxDB');
+      this.influx = new Influx.InfluxDB({
+            database: this.config.database,
+            host: this.config.host,
+            port: this.config.port,
+            username: this.config.username,
+            password: this.config.password,
+            schema: [
+               {
+               measurement: "sensor",
+               fields: {
+                  user :         FieldType.STRING,
+                  //timestamp :  FieldType.FLOAT,
+                  //mac:         FieldType.STRING,
+                  CO2:   FieldType.FLOAT,
+                  TVOC:    FieldType.FLOAT
+               },
+               tags: [ 'sensor' ]
+               }
+            ]
+         });
    };
 
-   save( jsonRecord, measurement ) {
-   	if (DEBUG) console.log('InfluxDB save function...');
+   save( jsonRecord ) {
+      if (DEBUG) console.log('InfluxDB save function...');
+
       this.influx.getDatabaseNames()
       .then(names => {
-       if ( !names.includes('datagas') ) {
-         if (DEBUG) console.log('First connection... create database datagas');
+       if ( !names.includes(this.config.database) ) {
+         if (DEBUG) console.log('First connection... create database '+ this.config.database);
          
          this.influx.createUser('test', 'test').then( ()=> {
-            return this.influx.createDatabase('datagas')
+            return this.influx.createDatabase(this.config.database)
          } );  
        }
       })
       .then( () => {
-         if (DEBUG) console.log('database : datagas found');
+         if (DEBUG) console.log('database : ' + this.config.database + ' found');
          if (DEBUG) console.log('jsonRecord = '+ JSON.stringify(jsonRecord) );
 
-         if       (measurement ==="sensor" ) { this.saveSensor(jsonRecord,measurement);  } 
-         else {
-            console.log('Mqtt message Type not managed... yet ;-) !!! ');
-         }
+         this.saveSensor(jsonRecord);
       })
       .catch(err => {
           console.error(`Error creating Influx database!`)
           console.log(`${err.stack}`);
           return;
       });
-   	// body...
+      // body...
    }
 
-   saveSensor(jsonRecord, measurement ) {    
+   saveSensor(jsonRecord, jsonPosition ) {    
       this.influx.writePoints([
          {
-         measurement: measurement,
-         tags: { sensor: "CCS811_Sensor" },
+         tags: { sensor: "teensySensors" },
+         measurement : "sensor",
          fields: { 
-            user :            jsonRecord.user,
-            timestamp :       Date.parse(jsonRecord.state.reported.timestamp),
-            CO2:              jsonRecord.CO2,
-            TVOC: jsonRecord.TVOC, 
+            user :         jsonRecord.user,
+            CO2:            jsonRecord.CO2,
+            TVOC:   jsonRecord.TVOC 
             }  
          }]).catch(err => {
             console.error(`Error saving Sensor data to InfluxDB! ${err.stack}`);
             return;
          }).then( () => {
-            console.log('Doc type '+measurement +' pushed to InFlux'  );
+            console.log('Data pushed to InFlux'  );
             console.log('\n');
          });
    }
-
-   saveJetPack(jsonRecord, measurement ) {
-      //Inverted case to considere...
-      var ActInverted = !(jsonRecord.state.reported.Act7)
-
-      console.log('Act7 = ' + jsonRecord.state.reported.Act7 );
-      console.log('ActInverted = ' + ActInverted );
-
-      this.influx.writePoints([
-         {
-         measurement: measurement,
-         tags: { jetpack: "CoolBoardJetpack" },
-         fields: { 
-            Act0_Value: convertBoolean(jsonRecord.state.reported.Act0),
-            Act1_Value: convertBoolean(jsonRecord.state.reported.Act1),
-            Act2_Value: convertBoolean(jsonRecord.state.reported.Act2),
-            Act3_Value: convertBoolean(jsonRecord.state.reported.Act3),
-            Act4_Value: convertBoolean(jsonRecord.state.reported.Act4),
-            Act5_Value: convertBoolean(jsonRecord.state.reported.Act5),
-            Act6_Value: convertBoolean(jsonRecord.state.reported.Act6),
-            Act6_Value: convertBoolean(jsonRecord.state.reported.Act6),
-            Act7_Value: convertBoolean(jsonRecord.state.reported.Act7)
-            }  
-         }]).catch(err => {
-            console.error(`Error saving Jetpack data to InfluxDB! ${err.stack}`);
-            return;
-         }).then( () => {
-            console.log('Doc type '+measurement +' pushed to InFlux' );
-            console.log('\n');
-         });
-   }
-}
-
-function convertBoolean( boolean ) {
-   if (DEBUG) console.log('convertBoolean entry '+  boolean );
-   var result = 0;
-   if (boolean) result = 1;
-   if (DEBUG) console.log('converted to '+ result );
-   return result;
 }
 
 module.exports = TamataInfluxDB;
--- systools/save_data.js
+++ systools/save_data.js
@@ -2,30 +2,30 @@
 - Listen to Mqtt Broker
 - On message
 - Analyse message Type ( Sensor, jetpack, ConfigSys, RtcConfig... )
+- Request for save on Mongo
 - Request for save on InFlux
 - If Internet connected, send to Cloud Mqtt Broker
 - Winston Logger to get last actions... ( Debug process )
 */
 var DEBUG = true;
-
+/* TEST */ 
 var jsonfile = require('jsonfile')
 jsonfile.spaces = 4;
 
 var mqtt = require('mqtt'); //includes mqtt server 
-
 const TamataInfluxDB = require('./actions/components/TamataInflux')
-var jsonConfig ;
 
-/* Config JSON indent mode */
-// configFile = "/home/pi/node/config/config.json";
-var configFile = "../config/config.json";
+var configFile = "config.json";
+var jsonConfig ;
 var mqttTopicIn="" 
 var mqttTopicOut="" 
 var mqttServer=""
 var mqttAWS= ""
-
 var influx;
 var mongo;
+var serialport;
+var baud;
+let parser;
 //---------------------
 //get config
 //---------------------
@@ -33,83 +33,50 @@ jsonfile.readFile(configFile, function(err, data) {
 		jsonConfig = data;
 
 		if (err) throw err;
-		mqttTopic = jsonConfig.system.mqttTopic + '/update';
-		mqttServer = jsonConfig.system.mqttServer;
-		mqttAWS = jsonConfig.system.mqttAWS;
-		user = jsonConfig.system.user;
+		mqttTopic = data.system.mqttTopic ;
+		mqttServer = data.system.mqttServer;
+		mqttUser = data.system.mqttUser;
+		mqttAWS = data.system.mqttAWS;
+		user = data.system.user;
 		begin();
 });
 
+/***************************************
+- function begin())
+- Listening on MQTT & Serial 
+- When MQTT message arrive, => insertData()
+ */
 function begin() {
 	if (DEBUG) {
-		// console.log('Function Begin(jsonConfig) : '+JSON.stringify(jsonConfig) );
-		console.log('Config');
-		console.log('MqttServer ='+ jsonConfig.system.mqttServer);
-		console.log('MqttTopic ='+ jsonConfig.system.mqttTopic);
-		console.log('Influx Config ='+ JSON.stringify(jsonConfig.system.influxDB) ) ;
+		console.log('.............. CONFIG .............');
+		console.log('MqttServer ='+ mqttServer);
+		console.log('MqttUser ='+ mqttUser);
+		console.log('MqttTopic ='+ mqttTopic);
+		console.log('InfluxDB ='+ JSON.stringify(jsonConfig.system.influxDB) ) ;
 	}
-   
+   	
+   	/* LISTENING on MQTT */
 	client = mqtt.connect('mqtt://'+ jsonConfig.system.mqttServer );
-    client.subscribe( jsonConfig.system.mqttTopic + "/update"); 
-    //client.subscribe("dev/update"); 
+    client.subscribe( jsonConfig.system.mqttTopic ); 
     client.on('connect', () => { console.log('Mqtt connected to ' + jsonConfig.system.mqttServer + "/ Topic : " + jsonConfig.system.mqttTopic  )} )
-    client.on('message', insertEvent );
+    client.on('message', insertData );
 }
 
-function insertEvent(topic,message) {
+/***************************************
+- function insertData(topic, message)
+Parse message & position and INSERT into Database */
+async function insertData(topic,message) {
 	var parsedMessage = JSON.parse(message);
 
-	if (DEBUG) console.log('************************');
-	if (DEBUG) console.log('Mqtt Message received : ');
-	if (DEBUG) console.log('Insert Message : ' + JSON.stringify(parsedMessage) ) ;
+	if (DEBUG) console.log('***********************************');
+	if (DEBUG) console.log('Mqtt Message received : ' + message );
 
-	// Checking Message 
-	const promiseMeasurement = new Promise( (resolve, reject) => {
-		return resolve();
-	})
-	// Then Connect to Moogose // Influx 
-	.then( getMeasurement(parsedMessage)
-		.then(  (measurement) => {
-			if (measurement !== "unManaged") {	
-				if (DEBUG) console.log('Begin saving... measurement = ' + measurement );
-				influx = new TamataInfluxDB( jsonConfig.system.influxDB, measurement );
-				influx.save( parsedMessage, measurement );
-			}
-			else {
-				if (DEBUG) console.log('UnManaged measurement = ' + measurement );
-			}
-		})
-	);
+	/* INSERT to influx database */
+	influx = new TamataInfluxDB( jsonConfig.system.influxDB );
+	influx.save( parsedMessage );
 
-	// Then Internet Check & Close DBs
-	promiseMeasurement.then( () => {
-		if (DEBUG) console.log('Last actions... ');
-	});
+	if (DEBUG) console.log('Inserted datas : ' + JSON.stringify(parsedMessage) ) ;
 
-} // End Insert Event
+} 
 
 
-/*********************************************
-- function getMeasurement(parsedMessage)
-Return a Promise with measurement type 
-sensor, jetpack, coolboardconfig, rtcconfig... 
-*/
-function getMeasurement(parsedMessage) {
-	return new Promise(  (resolve, reject) => {
-		return resolve();
-	}).then( () => {
-		if (DEBUG) {console.log('Function getMeasurement start... ');}
-		var measurement;
-
-		if ( parsedMessage.user === "GasStation" ) { 
-			if (DEBUG) { console.log('sensor message detected') }
-			measurement = "sensor";
-			return measurement;
-		} 
-		else {
-			return "unManaged";
-		}
-	});
-
-
-}
